aad5cf603ecaf71eef48bcf3e448d45f2b635822
RobbieClarken <robbie.clarken@gmail.com>
2013-10-06 03:24:52 +1100

    Create target image from user selected bounds

--- camreader.py
+++ camreader.py
@@ -6,6 +6,7 @@ import tempfile
 import numpy as np
 from scipy import misc, ndimage
 from matplotlib import pyplot
+from transform import regularize
 
 
 class Camera(object):
@@ -25,9 +26,12 @@ class Window(object):
         self.camera = camera
         figure = pyplot.figure()
         self.raw_axes = figure.add_subplot(121)
-        self.zoom_axes = figure.add_subplot(122)
-        self.im = None
-        self.target = None
+        self.target_axes = figure.add_subplot(122)
+        self.raw_data = None
+        self.raw_im = None
+        self.target_data = None
+        self.target_im = None
+        self.target_bounds = None
         self.target_vertices = None
         self.pressed_index = None
 
@@ -36,23 +40,31 @@ class Window(object):
         pyplot.show()
 
     def update(self):
-        img = self.camera.capture()
-        if self.im is None:
-            self.im = self.raw_axes.imshow(img)
+        self.raw_data = self.camera.capture()
+        if self.raw_im is None:
+            self.raw_im = self.raw_axes.imshow(self.raw_data)
             self.raw_axes.set_autoscale_on(False)
         else:
-            self.im.set_data(img)
+            self.raw_im.set_data(self.raw_data)
 
-        if self.target is None:
-            h, w = img.shape
+        if self.target_bounds is None:
+            h, w = self.raw_data.shape
             self.target_vertices = self.raw_axes.scatter([w/4, 3*w/4, 3*w/4, w/4],
                                                          [h/4, h/4, 3*h/4, 3*h/4])
-            self.target = pyplot.Polygon(self.target_vertices.get_offsets(), fill=False)
-            self.raw_axes.add_patch(self.target)
+            self.target_bounds = pyplot.Polygon(self.target_vertices.get_offsets(), fill=False)
+            self.raw_axes.add_patch(self.target_bounds)
             self.connect()
+            self.show_target_image()
 
     def show_target_image(self):
-        pass
+
+        pts = [(y,x) for x, y in self.target_vertices.get_offsets()]
+        self.target_data, transformed_pts = regularize(self.raw_data, pts)
+        if self.target_im is None:
+            self.target_im = self.target_axes.imshow(self.target_data)
+        else:
+            self.target_im.set_data(self.target_data)
+            self.target_im.figure.canvas.draw()
 
     def connect(self):
         self.press_cid = self.target_vertices.figure.canvas.mpl_connect(
@@ -72,6 +84,7 @@ class Window(object):
     def on_release(self, event):
         self.pressed_index = None
         self.target_vertices.figure.canvas.draw()
+        self.show_target_image()
 
     def on_motion(self, event):
         if self.pressed_index is None:
@@ -80,7 +93,7 @@ class Window(object):
             return
         pts = self.target_vertices.get_offsets()
         pts[self.pressed_index] = (event.xdata, event.ydata)
-        self.target.set_xy(pts)
+        self.target_bounds.set_xy(pts)
         self.target_vertices.figure.canvas.draw()
 
 
