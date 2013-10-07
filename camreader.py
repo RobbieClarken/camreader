ebb41ae8099db556fb45f65da7dff0cafda667b5
RobbieClarken <robbie.clarken@gmail.com>
2013-10-08 03:21:52 +1100

    Transform target section of image using projective geometry

--- camreader.py
+++ camreader.py
@@ -3,9 +3,36 @@
 import argparse
 import requests
 import tempfile
-from scipy import misc
-from matplotlib import pyplot
-from transform import regularize
+import numpy as np
+from scipy import misc, ndimage
+from matplotlib import pyplot, cm
+
+
+def rectify(image, world_points, image_points):
+
+    A = []
+    for world_pt, image_pt in zip(world_points, image_points):
+        x, y = world_pt
+        xp, yp = image_pt
+        A.append([x, y, 1, 0, 0, 0, -xp * x, -xp * y, -xp])
+        A.append([0, 0, 0, x, y, 1, -yp * x, -yp * y, -yp])
+    u, s, vh = np.linalg.svd(A)
+    transform_matrix = np.matrix(vh[-1].reshape(3, 3))
+
+    coords = np.argwhere(image > -1)
+    ones = np.matrix(np.repeat(1, coords.shape[0]))
+    projective_coords = np.append(coords.T, ones, axis=0)
+    world_coords = transform_matrix * projective_coords
+    world_coords = (world_coords / world_coords[-1, :]).astype(int)
+
+    height, width = image.shape
+
+    def transform(world_pt):
+        r, c = world_pt
+        idx = width * r + c
+        return (world_coords[0, idx], world_coords[1, idx])
+
+    return ndimage.interpolation.geometric_transform(image, transform)
 
 
 class Camera(object):
@@ -41,15 +68,15 @@ class Window(object):
     def update(self):
         self.raw_data = self.camera.capture()
         if self.raw_im is None:
-            self.raw_im = self.raw_axes.imshow(self.raw_data)
+            self.raw_im = self.raw_axes.imshow(self.raw_data, cmap=cm.Greys_r)
             self.raw_axes.set_autoscale_on(False)
         else:
             self.raw_im.set_data(self.raw_data)
 
         if self.target_bounds is None:
             h, w = self.raw_data.shape
-            self.target_vertices = self.raw_axes.scatter([w/4, 3*w/4, 3*w/4, w/4],
-                                                         [h/4, h/4, 3*h/4, 3*h/4])
+            self.target_vertices = self.raw_axes.scatter([w/4, w/4, 3*w/4, 3*w/4],
+                                                         [h/4, 3*h/4, 3*h/4, h/4])
             self.target_bounds = pyplot.Polygon(self.target_vertices.get_offsets(), fill=False)
             self.raw_axes.add_patch(self.target_bounds)
             self.connect()
@@ -57,10 +84,13 @@ class Window(object):
 
     def show_target_image(self):
 
-        pts = [(y,x) for x, y in self.target_vertices.get_offsets()]
-        self.target_data, transformed_pts = regularize(self.raw_data, pts)
+        image_points = [(y,x) for x, y in self.target_vertices.get_offsets()]
+        height, width = self.raw_data.shape
+        world_points = [(0, 0), (height, 0), (height, width), (0, width)]
+        self.target_data = rectify(self.raw_data, world_points, image_points)
+
         if self.target_im is None:
-            self.target_im = self.target_axes.imshow(self.target_data)
+            self.target_im = self.target_axes.imshow(self.target_data, cmap=cm.Greys_r)
         else:
             self.target_im.set_data(self.target_data)
             self.target_im.figure.canvas.draw()
